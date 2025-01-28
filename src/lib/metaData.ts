094beadd66c3168aea04f67c43275c083b47e509
Fabian <fabianriveraabian3@gmail.com>
2025-01-28 01:51:43 -0500

    cambio de imgOg

--- src/lib/metaData.ts
+++ src/lib/metaData.ts
@@ -4,7 +4,7 @@ export const metaData = {
     description: "A portfolio page built with React and Vite, showcasing projects and skills with a modern and efficient interface.",
     favicon: '/Profile.webp',
     color: "#181c14",
-    ogImage: 'https://portafolio-fbn-1.netlify.app/Capture.webp',
+    ogImage: 'https://opengraph.b-cdn.net/production/images/32960fad-be85-492a-baf3-a9936be3fff4.png?token=ZNNsVcURovNw82DFqt6Jyw7-5Y3wyrBbbw2QDFx2SiQ&height=798&width=1200&expires=33274046905',
     url: 'https://portafolio-fbn-1.netlify.app/',
     siteName: 'Portafolio motion Fabian1803',
     twitterHandle: '@Fabian1803',
